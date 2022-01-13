ca33a9793400a000970646234cafb984fac760ed
ESIEA-Dive <dive@et.esiea.fr>
2022-01-13 21:49:28 +0100

    Retrofit Cache

--- site/src/main/java/fr/lernejo/travelsite/Launcher.java
+++ site/src/main/java/fr/lernejo/travelsite/Launcher.java
@@ -1,10 +1,17 @@
 package fr.lernejo.travelsite;
 
 import fr.lernejo.travelsite.api.PredictionClient;
+import okhttp3.Cache;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.io.File;
 
 @SpringBootApplication
 public class Launcher {
@@ -15,10 +22,34 @@ public class Launcher {
 
     @Bean
     PredictionClient predictionClient() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .cache(createCache())
+            .addNetworkInterceptor(createNetworkInterceptor())
+            .build();
+
         Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://localhost:7080/")
+            .client(okHttpClient)
+            .addConverterFactory(JacksonConverterFactory.create())
             .build();
 
         return retrofit.create(PredictionClient.class);
+    }
+
+    private Interceptor createNetworkInterceptor() {
+        return chain -> {
+            Response originalResponse = chain.proceed(chain.request());
+            return originalResponse.newBuilder()
+                .header("Cache-Control", "max-age=120, only-if-cached, max-stale=0")
+                .build();
+        };
+    }
+
+    private Cache createCache() {
+        return new Cache(getCacheDirectory(), 1024 * 1024 * 1024);
+    }
+
+    private File getCacheDirectory() {
+        return new File("./cache/predictionEngineResponse");
     }
 }
